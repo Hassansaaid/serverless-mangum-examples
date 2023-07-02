65a8fdbc810c4ca05784bb9ff64b432591288927
hassan_saaid <hassan_saaid@carrefour.com>
2023-07-02 18:45:54 +0100

    test2

--- django-example/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from .views import HelloWorldView
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("hello/", HelloWorldView.as_view()),
-]
